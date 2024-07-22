d3d8a081f5c032f4b021852462201ec0d0afede2
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-07-22 21:18:12 +0900

    프록시 공부
    프록시 공부 자료

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,9 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.advice.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +14,11 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import(AppV2Config.class)
 //@Import({AppV2Config.class,InterfaceProxyConfig.class})
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import({DynamicProxyFilterConfig.class,AppV2Config.class})
+// @Import({ProxyFactoryConfigV1.class,AppV2Config.class})
+//@Import(ProxyFactoryConfigV2.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 	public static void main(String[] args) {
